5c697ce158b18bb92b30b2d485d76ece9c70f6be
spencercreer <spencercreer@gmail.com>
2021-04-04 13:36:55 -0600

    added update employee SQL update

--- db/index.js
+++ db/index.js
@@ -16,17 +16,18 @@ var connection = mysql.createConnection({
 
 connection.query = util.promisify(connection.query);
 
+// findAll functions query mysql for employees, roles, and departments
 function findAllEmployees() {
     return connection.query('SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN roles on employee.role_id = roles.id LEFT JOIN department on roles.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id')
-}
-function findAllDepartments() {
-    return connection.query('SELECT department.id, department.name FROM department LEFT JOIN roles ON roles.department_id = department.id LEFT JOIN employee ON employee.role_id = roles.id GROUP BY department.id, department.name')
 }
 function findAllRoles() {
     return connection.query('SELECT roles.id, roles.title, roles.salary, department.name FROM roles LEFT JOIN department ON roles.department_id = department.id')
 }
+function findAllDepartments() {
+    return connection.query('SELECT department.id, department.name FROM department LEFT JOIN roles ON roles.department_id = department.id LEFT JOIN employee ON employee.role_id = roles.id GROUP BY department.id, department.name')
+}
 
-// addEmployee function prompts user for employee info and INSERTS into db
+// addEmployee function prompts user employee questions and INSERTS into mysql db
 function addEmployee() {
     inquirer.prompt(questions.employeeQuestions).then(response => {
         findAllRoles().then(data => {
@@ -55,7 +56,7 @@ function addEmployee() {
     });
 }
 
-// addRole function prompts user for role info and INSERTS into db
+// addRole function prompts user roleQuestions and INSERTS into db mysql
 function addRole() {
     inquirer.prompt(questions.roleQuestions).then(response => {
         findAllDepartments().then(data => {
@@ -84,7 +85,7 @@ function addRole() {
     });
 }
 
-// addDepartment function prompts user for department info and INSERTS into db
+// addDepartment function prompts user departmentQuestions and INSERTS into mysql db
 function addDepartment() {
     inquirer.prompt(questions.departmentQuestions).then(response => {
         connection.query('INSERT INTO department SET ?',
@@ -102,6 +103,7 @@ function addDepartment() {
     });
 }
 
+// removeEmployee function prompts user to select an employee to delete from the mysql db
 function removeEmployee() {
     findAllEmployees().then(data => {
         inquirer.prompt([
@@ -116,7 +118,7 @@ function removeEmployee() {
             connection.query('DELETE FROM employee WHERE ?',
                 {
                     id: data.filter(employeeData => {
-                        if(employeeData.first_name === removeEmployee.split(" ")[0] && employeeData.last_name === removeEmployee.split(" ")[1]){
+                        if (employeeData.first_name === removeEmployee.split(" ")[0] && employeeData.last_name === removeEmployee.split(" ")[1]) {
                             return true
                         }
                     })[0].id,
@@ -126,9 +128,59 @@ function removeEmployee() {
                     console.log('\x1b[32m%s\x1b[0m', `${removeEmployee} was successfully removed`);
                     init();
                 })
-            )
-        });
+        )
+    });
 }
+
+// updateEmployee function prompts user to select an employee to update in the mysql db
+function updateEmployee() {
+    findAllEmployees().then(findAllEmployeeResponse => {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'updateEmployee',
+                message: 'Select an employee to update',
+                choices: findAllEmployeeResponse.map(employeeData => employeeData.first_name.concat(" ", employeeData.last_name))
+            }
+        ]).then(({ updateEmployee }) => {
+            inquirer.prompt(questions.employeeQuestions).then(response => {
+                findAllRoles().then(findAllRolesResponse => {
+                    inquirer.prompt([
+                        {
+                            type: 'list',
+                            name: 'employeeRole',
+                            message: 'What is the employee role title',
+                            choices: findAllRolesResponse.map(roleData => roleData.title)
+                        }
+                    ]).then(({ employeeRole }) =>
+                        connection.query('UPDATE employee SET ? WHERE ?',
+                            [
+                                {
+                                    first_name: response.firstName,
+                                    last_name: response.lastName,
+                                    role_id: findAllRolesResponse.filter(roleData => roleData.title === employeeRole)[0].id,
+                                    manager_id: 1
+                                },
+                                {
+                                    id: findAllEmployeeResponse.filter(employeeData => {
+                                        if (employeeData.first_name === updateEmployee.split(" ")[0] && employeeData.last_name === updateEmployee.split(" ")[1]) {
+                                            return true
+                                        }
+                                    })[0].id,
+                                },
+                            ],
+                            (err) => {
+                                if (err) throw err;
+                                console.log('\x1b[32m%s\x1b[0m', `${response.firstName.concat(" ", response.lastName)} was successfully updated`);
+                                init();
+                            })
+                    )
+                })
+            })
+        })
+    })
+}
+
 
 // initiate function prompts the user what action they would like to perform
 function init() {
@@ -162,6 +214,7 @@ function init() {
         } else if (response.userChoice === 'Remove an employee') {
             removeEmployee();
         } else if (response.userChoice === 'Update an employee') {
+            updateEmployee();
         } else {
             console.log('\x1b[31m%s\x1b[0m', '\nProgram ended\n');
             process.exit(1);
@@ -169,6 +222,7 @@ function init() {
     })
 }
 
+// connect mysql connection
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`);
